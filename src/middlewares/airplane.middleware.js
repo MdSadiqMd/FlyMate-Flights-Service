ef06bc5d2b09839f0a7028b4e97d67cf178a5a0f
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-07-26 20:44:13 +0530

    Feat: Added Routes with middlewares

---
+++ src/middlewares/airplane.middleware.js
@@ -0,0 +1,20 @@
+const { StatusCodes } = require("http-status-codes");
+
+const { ErrorResponse } = require("../utils");
+const AppError = require("../errors/App.error");
+
+function validateCreateRequest(req, res, next) {
+  if (!req.body.modelNumber) {
+    ErrorResponse.message = "Something went wrong while creating airplane";
+    ErrorResponse.error = new AppError(
+      ["Model Number not found in the oncoming request in the correct form"],
+      StatusCodes.BAD_REQUEST
+    );
+    return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+  }
+  next();
+}
+
+module.exports = {
+  validateCreateRequest,
+};
---
+++ src/routes/v1/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const airplaneRoutes = require("./airplane.routes");
+
+const router = express.Router();
+
+router.use("/airplanes", airplaneRoutes);
+
+module.exports = router;
