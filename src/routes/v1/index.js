9f6e66ae7af42dec6055b00b979152fffb75fec0
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-12-26 21:52:18 +0530

    Added info controller

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,6 +4,7 @@ const airplaneRoutes = require("./airplane.routes");
 const cityRoutes = require("./city.routes");
 const airportRoutes = require("./airport.routes");
 const flightsRoutes = require("./flight.routes");
+const { info } = require("../../controllers/info.controller");
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use("/airplanes", airplaneRoutes);
 router.use("/cities", cityRoutes);
 router.use("/airports", airportRoutes);
 router.use("/flights", flightsRoutes);
+router.get('/info', info);
 
 module.exports = router;
